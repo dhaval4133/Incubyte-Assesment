2dfdce3c9609888fc86eddcc9d0f0482f592a120
Dhaval Dangar <123232033+dhaval4133@users.noreply.github.com>
2025-07-16 15:23:40 +0530

    Add test for addSweet method

--- src/test/java/com/sweetshop/SweetShopTest.java
+++ src/test/java/com/sweetshop/SweetShopTest.java
@@ -5,5 +5,14 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 public class SweetShopTest {
+    @Test
+    public void testAddSweet() {
+        SweetShop shop = new SweetShop();
+        Sweet sweet = new Sweet(1001, "Kaju Katli", "Nut-Based", 50, 20);
+        shop.addSweet(sweet);
+        assertEquals(1, shop.getAllSweets().size());
+        assertEquals("Kaju Katli", shop.getAllSweets().get(0).getName());
+    }
+
 
 }
