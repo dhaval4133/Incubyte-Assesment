985941cf99769efd8f8b82aef35b5240d253ff08
Dhaval Dangar <123232033+dhaval4133@users.noreply.github.com>
2025-07-16 15:29:16 +0530

    Add test for getAllSweets method

--- src/test/java/com/sweetshop/SweetShopTest.java
+++ src/test/java/com/sweetshop/SweetShopTest.java
@@ -13,6 +13,16 @@ public class SweetShopTest {
         assertEquals(1, shop.getAllSweets().size());
         assertEquals("Kaju Katli", shop.getAllSweets().get(0).getName());
     }
-
+    @Test
+    public void testViewAllSweets() {
+        SweetShop shop = new SweetShop();
+        Sweet sweet1 = new Sweet(1001, "Kaju Katli", "Nut-Based", 50, 20);
+        Sweet sweet2 = new Sweet(1002, "Gajar Halwa", "Vegetable-Based", 30, 15);
+        shop.addSweet(sweet1);
+        shop.addSweet(sweet2);
+        assertEquals(2, shop.getAllSweets().size());
+        assertTrue(shop.getAllSweets().contains(sweet1));
+        assertTrue(shop.getAllSweets().contains(sweet2));
+    }
 
 }
